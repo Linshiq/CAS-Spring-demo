f8d84de9237f1ebd75a05c7dc3fa78f090185a1c
Linshiq <284244633@qq.com>
2020-01-14 15:29:11 +0800

    代码初始化提交

---
+++ cust2-web/src/main/java/com/lsq/lsqweb/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.lsq.lsqweb.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class TestController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
+	public String test() throws Exception {
+		
+		logger.info("测试");
+		
+		return "测试";
+	}
+}
--- sso-web/src/main/java/com/lsq/lsqweb/LsqWebApplication.java
+++ sso-web/src/main/java/com/lsq/lsqweb/LsqWebApplication.java
@@ -1,9 +1,12 @@
 package com.lsq.lsqweb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) // 需要连接数据库就把这个注释去掉
 public class LsqWebApplication {
 
 	public static void main(String[] args) {
